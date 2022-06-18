bb7e7c214dac71f3d77dbc0ec38c1f0292d6db11
drg <drgsan@yahoo.com>
2022-06-18 10:22:46 +0300

    Added func "add_vpn_user", and "delete_sert_files"; Changed func "copy_sert"

--- apps/vpn.py
+++ apps/vpn.py
@@ -7,7 +7,12 @@ class VPN:
         self.remote_dir = remote_dir
         self.local_dir = local_dir
 
+    def add_vpn_user(self, user_name):
+        os.system(f'ssh {self.server_host} "sudo ikev2.sh --addclient {user_name}"')
+
     def copy_sert(self, user_name):
+        os.system(f'ssh {self.server_host} "sudo ikev2.sh --addclient {user_name}"')
+
         os.system(f'ssh {self.server_host} "sudo ikev2.sh --exportclient {user_name}"')
         os.system(f'scp {self.server_host}:{self.remote_dir}/{user_name}.mobileconfig '
                   f'{self.local_dir}/{self.server_host}-{user_name}.mobileconfig')
@@ -15,6 +20,8 @@ class VPN:
                   f'{self.local_dir}/{self.server_host}-{user_name}.p12')
         os.system(f'scp {self.server_host}:{self.remote_dir}/{user_name}.sswan '
                   f'{self.local_dir}/{self.server_host}-{user_name}.sswan')
+
+    def delete_sert_files(self, user_name):
         os.system(f'ssh {self.server_host} "rm {user_name}*"')
 
 
